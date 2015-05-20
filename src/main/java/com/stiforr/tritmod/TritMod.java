19dfc2dcf7471ba74ade7d4623a44cb94dd9bf75
Sean <norwood.sean@gmail.com>
2015-05-19 21:23:05 -0500

    Adding all the things

---
+++ src/main/java/com/stiforr/tritmod/TritMod.java
@@ -0,0 +1,30 @@
+package com.stiforr.tritmod;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+
+@Mod(modid="TritMod", name="Trit Mod", version="1.7.10-1.0")
+public class TritMod 
+{
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		
+	}
+	
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		
+	}
+	
+	@Mod.EventHandler
+	public void init(FMLPostInitializationEvent event)
+	{
+		
+	}
+}
