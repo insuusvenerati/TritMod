4d0d0c6f8caf48e48b607647d3af8ab946017862
Sean <norwood.sean@gmail.com>
2015-05-21 15:38:03 -0500

    More random stuff
    Added config gui
    Added log levels
    Added more stuff to reference class

--- src/main/java/com/stiforr/tritmod/TritMod.java
+++ src/main/java/com/stiforr/tritmod/TritMod.java
@@ -1,7 +1,9 @@
 package com.stiforr.tritmod;
 
+import com.stiforr.tritmod.handler.ConfigHandler;
 import com.stiforr.tritmod.proxy.IProxy;
 import com.stiforr.tritmod.reference.Reference;
+import com.stiforr.tritmod.utility.LogHelper;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
@@ -11,31 +13,32 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 
-@Mod(modid=Reference.MOD_ID, name=Reference.MOD_NAME, version=Reference.VERSION)
+@Mod(modid=Reference.MOD_ID, name=Reference.MOD_NAME, version=Reference.VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)
 public class TritMod 
 {
 	
 	@Mod.Instance(Reference.MOD_ID)
 	public static TritMod instance;
 	
-	@SidedProxy(clientSide = "com.stiforr.tritmod.proxy.ClientProxy", serverSide = "com.stiforr.tritmod.proxy.ServerProxy")
+	@SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
 	public static IProxy proxy;
 	
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event)
 	{
-		
+		ConfigHandler.init(event.getSuggestedConfigurationFile());
+		LogHelper.info("Pre Init Complete");
 	}
 	
 	@Mod.EventHandler
 	public void init(FMLInitializationEvent event)
 	{
-		
+		LogHelper.info("Init Complete");
 	}
 	
 	@Mod.EventHandler
 	public void postinit(FMLPostInitializationEvent event)
 	{
-		
+		LogHelper.info("Post Init Complete");
 	}
 }
---
+++ src/main/java/com/stiforr/tritmod/client/gui/ModGuiConfig.java
@@ -0,0 +1,22 @@
+package com.stiforr.tritmod.client.gui;
+
+import com.stiforr.tritmod.handler.ConfigHandler;
+import com.stiforr.tritmod.reference.Reference;
+
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.common.config.ConfigElement;
+import net.minecraftforge.common.config.Configuration;
+import cpw.mods.fml.client.config.GuiConfig;
+
+public class ModGuiConfig extends GuiConfig
+{
+	public ModGuiConfig(GuiScreen guiScreen)
+	{
+		super(guiScreen,
+			new ConfigElement(ConfigHandler.configuration.getCategory(Configuration.CATEGORY_GENERAL)).getChildElements(),
+			Reference.MOD_ID,
+			false,
+			false,
+			GuiConfig.getAbridgedConfigPath(ConfigHandler.configuration.toString()));
+	}
+}
