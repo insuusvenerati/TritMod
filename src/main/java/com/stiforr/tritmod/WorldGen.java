fb7cc1fad662eac368b99abea8e0c9e8b1bc4725
Sean <norwood.sean@gmail.com>
2015-05-23 20:23:54 -0500

    idk what i'm doing lol

--- src/main/java/com/stiforr/tritmod/WorldGen.java
+++ src/main/java/com/stiforr/tritmod/WorldGen.java
@@ -2,12 +2,18 @@ package com.stiforr.tritmod;
 
 import java.util.Random;
 
+import net.minecraft.block.Block;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.feature.WorldGenMinable;
 import cpw.mods.fml.common.IWorldGenerator;
 
 public class WorldGen implements IWorldGenerator
+
+
 	{
+	
+	
 	
 	@Override
 	public void generate(Random random, int chunkX, int chunkZ, World world,
@@ -37,7 +43,10 @@ public class WorldGen implements IWorldGenerator
 	{
 		for(int k = 0; k < 10; k++) 
 		{
-		    
+			int tritOreXCoord = i + random.nextInt(16);
+			int tritOreYCoord = random.nextInt(20);
+			int tritOreZCoord = j + random.nextInt(16);
+			WorldGenMinable(Block.tritOre, 0)).generate(world, random, firstBlockXCoord, firstBlockYCoord, firstBlockZCoord);
 		}
 		
 	}
